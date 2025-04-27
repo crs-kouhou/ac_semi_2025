9b673ef4a12c9c4a820cdfe0db8104ec65919330
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-04-27 17:14:49 +0900

    時間計測を追加

--- src/icp_test.cpp
+++ src/icp_test.cpp
@@ -3,6 +3,7 @@
 #include <limits>
 #include <print>
 #include <iostream>
+#include <chrono>
 
 #include <eigen3/Eigen/Dense>
 
@@ -22,8 +23,8 @@ using namespace ac_semi_2025::read_edges;
 /// @brief 点群を線分群にfittingする ICP on SVD
 /// 線分数は点数に比べ十分少ないとする
 inline auto icp_p2l(const Matrix2Xd& from, std::vector<Line2d> to, const i64 number_of_iteration) noexcept -> Pose2d {
-	// 出力
-	std::println("{}", number_of_iteration);
+	// // 出力
+	// std::println("{}", number_of_iteration);
 
 	// fromを、重心を原点とする座標系に変換したものを用意
 	auto from_ = from;
@@ -35,10 +36,10 @@ inline auto icp_p2l(const Matrix2Xd& from, std::vector<Line2d> to, const i64 num
 	auto closest_points = Matrix2Xd{2, from.cols()};
 	auto total_transform = Isometry2d::Identity();
 	for(i64 iloop = 0; iloop < number_of_iteration; iloop++) {
-		std::println("{}", to.size());
-		for(const auto& toi : to) {
-			std::println("{} {} {} {}", toi.p1(0), toi.p2(0), toi.p1(1), toi.p2(1));
-		}
+		// std::println("{}", to.size());
+		// for(const auto& toi : to) {
+		// 	std::println("{} {} {} {}", toi.p1(0), toi.p2(0), toi.p1(1), toi.p2(1));
+		// }
 
 		// fromの各点の最近接点を求める
 		for(i64 ip = 0; ip < i64(from.cols()); ++ip) {
@@ -55,10 +56,10 @@ inline auto icp_p2l(const Matrix2Xd& from, std::vector<Line2d> to, const i64 num
 			closest_points.col(ip) = closest_point;
 		}
 
-		std::println("{}", closest_points.cols());
-		for(i64 i = 0; i < closest_points.cols(); ++i) {
-			std::println("{} {}", closest_points.col(i)(0), closest_points.col(i)(1));
-		}
+		// std::println("{}", closest_points.cols());
+		// for(i64 i = 0; i < closest_points.cols(); ++i) {
+		// 	std::println("{} {}", closest_points.col(i)(0), closest_points.col(i)(1));
+		// }
 
 		// closest_pointsを、重心を原点とする座標系に直す
 		const auto closest_points_mean = closest_points.rowwise().mean().eval();
@@ -79,12 +80,6 @@ inline auto icp_p2l(const Matrix2Xd& from, std::vector<Line2d> to, const i64 num
 		if(optimized_rotation.determinant() < 0.0) {
 			optimized_rotation(1, 1) = -optimized_rotation(1, 1);
 		}
-		// auto optimized_rotation = (v * u.transpose()).eval();
-		// if (optimized_rotation.determinant() < 0.0) {
-		// 	Matrix2d reflect = Matrix2d::Identity();
-		// 	reflect(1,1) = -1;
-		// 	optimized_rotation = v * reflect * u.transpose();
-		// }
 
 		// 最適な平行移動を計算
 		auto optimized_translation = closest_points_mean - optimized_rotation * from_mean;
@@ -93,9 +88,9 @@ inline auto icp_p2l(const Matrix2Xd& from, std::vector<Line2d> to, const i64 num
 		// 最適な剛体変換に合わせ、それを蓄積する
 		auto optimized_transform = Isometry2d::Identity();
 		optimized_transform.rotate(optimized_rotation).pretranslate(optimized_translation);
-		std::println(std::cerr, "translate: {} {}", optimized_translation(0), optimized_translation(1));
-		std::println(std::cerr, "from_mean: {} {}", from_mean(0), from_mean(1));
-		std::println(std::cerr, "closest_points_mean: {} {}", closest_points_mean(0), closest_points_mean(1));
+		// std::println(std::cerr, "translate: {} {}", optimized_translation(0), optimized_translation(1));
+		// std::println(std::cerr, "from_mean: {} {}", from_mean(0), from_mean(1));
+		// std::println(std::cerr, "closest_points_mean: {} {}", closest_points_mean(0), closest_points_mean(1));
 
 		total_transform = optimized_transform * total_transform;
 
@@ -135,7 +130,7 @@ int main() {
 		else return std::numeric_limits<double>::infinity();
 	};
 
-	const i64 m = 10;
+	const i64 m = 20;
 	std::vector<Line2d> l(m);
 	l[0] = Line2d {
 		Vector2d{-3.1, -1.1}
@@ -161,7 +156,7 @@ int main() {
 		l[i] = Line2d{gen_vec2d(eng), gen_vec2d(eng)};
 	}
 
-	const i64 n = 1000;
+	const i64 n = 2000;
 	Matrix2Xd p{2, n};
 	for(i64 i = 0; i < n; ++i) {
 		const double th = 2 * pi / n * i;
@@ -189,16 +184,19 @@ int main() {
 	
 	std::uniform_real_distribution<> err_th{0.0, 2 * pi};
 	const auto err_trans = Pose2d {
-		Vector2d{std::cos(err_th(eng)), std::sin(err_th(eng))} * 0.05
-		, err_th(eng) / 360.0 * 5
+		Vector2d{std::cos(err_th(eng)), std::sin(err_th(eng))} * 0.5
+		, err_th(eng) / 360.0 * 30
 	}.homogeneous_transform();
 
 	const auto points = err_trans * p;
 	static_assert(decltype(points)::RowsAtCompileTime == 2);
-	std::println("{}", n);
-	for(i64 i = 0; i < n; ++i) {
-		const auto pi = points.col(i);
-		std::println("{} {}", pi(0), pi(1));
-	}
+	// std::println("{}", n);
+	// for(i64 i = 0; i < n; ++i) {
+	// 	const auto pi = points.col(i);
+	// 	std::println("{} {}", pi(0), pi(1));
+	// }
+	const auto start = std::chrono::high_resolution_clock::now();
 	icp_p2l(points, l, 50);
+	const auto end = std::chrono::high_resolution_clock::now();
+	std::println(std::cerr, "end: {}", std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
 }
