0b1b26a9e4973e79caa68d8c258adba90bba61e5
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-04-29 03:42:26 +0900

    base_linkからlaserへのstaticなtransformを吐く

---
+++ launch/static_trasform.bash
@@ -0,0 +1,2 @@
+. /opt/ros/jazzy/setup.bash
+ros2 run tf2_ros static_transform_publisher --x 0.5 --y 0.0 --z 0.0 --yaw 0.0 --pitch 0.0 --roll 3.14159265358979329 --frame-id base_link --child-frame-id laser
