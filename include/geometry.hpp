e65131f58153017d55281eaf25011c3aa88829e3
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-05-07 00:33:23 +0900

    いらない部分を削除

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -17,7 +17,6 @@ namespace ac_semi_2025::geometry::impl {
 	concept edge = requires(const T_ e1, const T_ e2, const Vector2d p) {
 		{intersect(e1, e2)} -> std::same_as<std::vector<Vector2d>>;
 		{closest_p2e(p, e1)} -> std::same_as<std::pair<Vector2d, double>>;
-		{closest_e2e(e1, e2)} -> std::same_as<std::tuple<Vector2d, Vector2d, double>>;
 	};
 
 	// 姿勢
@@ -84,20 +83,6 @@ namespace ac_semi_2025::geometry::impl {
 		}
 	}
 
-	inline constexpr auto closest_e2e(const Line2d& part, const Line2d& whole) noexcept -> std::tuple<Vector2d, Vector2d, double> {
-		/// @todo
-		/// partを含む直線にwholeの端点からwholeに直交する向きにおろした2直線とpartを含む直線との交点p, qを考え、
-		/// pからqの範囲内では「wholeを含む直線との距離」を、範囲外では「wholeの端点との距離」をpartに沿って線積分する
-		/// 直線との距離の部分は1次関数の積分なので簡単
-		/// 問題は端点との距離のほう。積分すると\int_a^b \sqrt{x^2 + h^2} dxとなる。
-		/// ここでxはwholeの端点からpartに落とした垂線の足を原点にとったpartに沿う数直線で、a, bはpartの端点、pかqのx軸上の位置。
-		/// 積分は計算すると\frac{h \sqrt{x^2 + h^2}}{2} + \frac{h^2}{4} \log{\frac{\sqrt{x^2 + h^2} + x}{\sqrt{x^2 + h^2} - x}}
-		/// となる、はず
-		(void) part;
-		(void) whole;
-		return {};  // 仮
-	}
-
 	/// @brief 直線
 	struct Ray2d final {
 		Vector2d start;
@@ -148,5 +133,4 @@ namespace ac_semi_2025::geometry {
 	using impl::Polyline2d;
 	using impl::Polygon2d;
 	using impl::closest_p2e;
-	using impl::closest_e2e;
 }
