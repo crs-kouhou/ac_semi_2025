47808de44039f9faeb136e1fd7986c69afe4fa58
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-05-09 16:34:31 +0900

    リファクタリグ+差分の積分にclampを追加

--- include/carrot_pursuit.hpp
+++ include/carrot_pursuit.hpp
@@ -14,71 +14,88 @@ namespace ac_semi_2025::carrot_pursuit::impl {
 
 	using geometry::Pose2d;
 
-	struct PID {
-		double K_P = 0.0;
-		double K_I = 0.0;
-		double K_D = 0.0;
-		double last_P = 0.0;
-		double last_integrated_P = 0.0;
-		double feedforward = 0.0;
+	inline auto clamp(const double x, const double min, const double max) -> double {
+		return std::max(std::min(x, max), min);
+	}
 
+	struct FeedForwardedPid final {
+		struct Constant final {
+			double kp;
+			double ki;
+			double kd;
+			double i_max;
+			double feed_forward;
+		};
 
-		void make(double K_P, double K_I, double K_D) noexcept {
-			this->K_P = K_P;
-			this->K_I = K_I;
-			this->K_D = K_D;
+		const Constant cons;
+		double last_p{0.0};
+		double last_i{0.0};
+
+		static auto make(const Constant& cons) noexcept -> FeedForwardedPid {
+			return FeedForwardedPid{cons};
 		}
-		void make(double K_P, double K_I, double K_D,double feedforward) noexcept {
-			this->K_P = K_P;
-			this->K_I = K_I;
-			this->K_D = K_D;
-			this->feedforward = feedforward;
-		}
-		auto update(double P,double dt) noexcept -> double {
-			auto dP = (P - this->last_P) / dt;
-			auto integrated_P = this->last_integrated_P + P * dt;
-			auto ret = this->K_P * P + this->K_I * integrated_P + this->K_D * dP + this->feedforward;
-			this->last_P = P;
-			this->last_integrated_P = integrated_P;
+
+		auto update(const double p, const double dt) noexcept -> double {
+			const auto d = (p - this->last_p) / dt;
+			const auto i = clamp(this->last_i + p * dt, -this->cons.i_max, this->cons.i_max);
+			const auto ret = this->cons.kp * p + this->cons.ki * i + this->cons.kd * d + this->cons.feed_forward;
+			this->last_p = p;
+			this->last_i = i;
 			return ret;
 		}
-
-		
 	};
 
-	
-
+	// 現在位置と目標位置との差分を、ルート上の目標位置近傍での局所座標系で表し、
+	// 成分ごとにFFつきPID制御器に掛け、これを制御入力とする。
+	// なお、機体が戻る方向に動かないよう、目標位置の更新は工夫している
+	//
+	// 以降、局所座標系の接線方向をs, 法線方向をtとする
 	struct CarrotPursuit final {
-		/// @todo 実装。configurableな項目を入れておく
-		// int distance_threshold;
-		// int speed_determination_destination;
-		PID PID_x;
-		PID PID_y;
+		FeedForwardedPid controller_s;
+		FeedForwardedPid controller_t;
+		i64 target_idx{1};
 
-		auto update(const std::vector<Vector2d> &route, const Pose2d &current_pose, i64 &closest_ever_idx , const double dt) noexcept -> std::optional<Pose2d> {
-			// 方向を求めるフェーズ
-			auto road_vector = (route[closest_ever_idx + 1] - route[closest_ever_idx]).eval();
-			auto distination_vector = (route[closest_ever_idx + 1] - current_pose.xy).eval();
-			while (road_vector.dot(distination_vector) < 0){
-				closest_ever_idx++;
-				if (closest_ever_idx >= route.size() - 1) {
-					return std::nullopt;
+		static auto make(const FeedForwardedPid::Constant& s, const FeedForwardedPid::Constant& t) noexcept -> CarrotPursuit {
+			return CarrotPursuit {
+				.controller_s = FeedForwardedPid{s}
+				, .controller_t = FeedForwardedPid{t}
+			};
+		}
+
+		auto update(const std::vector<Vector2d> &route, const Pose2d &current_pose, const double dt) noexcept -> std::optional<Pose2d> {
+			Vector2d unnormalized_s = (route[this->target_idx] - route[this->target_idx - 1]).eval();
+			Vector2d to_dest = (route[this->target_idx] - current_pose.xy).eval();
+			// this->target_idxは減ることはない。よって機体が軌道上を逆戻りすることはない
+			// 目標位置の接線ベクトルと現在位置と目標位置の差分ベクトルの内積の正負により、
+			// 目標位置を通過したかを判定できる
+			while(unnormalized_s.dot(to_dest) < 0) {
+				++this->target_idx;
+				if (i64(route.size()) <= this->target_idx) {  // ゴールしたら
+					// その場で停止(@todo: ホントはゴールへ近づき、ゴールとの距離や速度が落ち着いてから停止すべき)
+					return Pose2d{Vector2d::Zero(), 0.0};
 				}
-				road_vector = (route[closest_ever_idx + 1] - route[closest_ever_idx]).eval();
-				distination_vector = (route[closest_ever_idx + 1] - current_pose.xy).eval();
+				unnormalized_s = (route[this->target_idx] - route[this->target_idx - 1]).eval();
+				to_dest = (route[this->target_idx] - current_pose.xy).eval();
 			}
-			Eigen::Rotation2Dd _90degree_rotation(-M_PI / 2);
-			Eigen::Rotation2Dd degree90_rotation(M_PI / 2);
-			auto P_x = road_vector.dot(distination_vector) / road_vector.norm();
-			auto P_y = (road_vector.dot(distination_vector * _90degree_rotation.toRotationMatrix())) / road_vector.norm();
-			auto distination_vector_target =  PID_x.update(P_x, dt) * road_vector.normalized() + PID_y.update(P_y, dt) * degree90_rotation.toRotationMatrix()* road_vector.normalized();
+
+			// 目標位置近傍での局所座標系で成分表示
+			const auto s = unnormalized_s.normalized();
+			const auto t = Vector2d{-s(1), s(0)};
+			const auto p_s = s.dot(to_dest);
+			const auto p_t = t.dot(to_dest);
+
+			// 各成分を制御器にかけ、その出力を制御入力の機体速度とする
+			const double out_s = this->controller_s.update(p_s, dt);
+			const double out_t = this->controller_t.update(p_t, dt);
+			auto target_speed = out_s * s + out_t * t;
 			
-			return Pose2d{distination_vector_target,0.0};
+			return Pose2d{target_speed, 0.0};
 			
 		}
 	};
 }
 
 namespace ac_semi_2025::carrot_pursuit {
+	using impl::FeedForwardedPid;
 	using impl::CarrotPursuit;
 }
