414674cd14186f8edcc20d683a313855e35a15eb
Stew-000-1-0-011 <stew00010011@gmail.com>
2025-05-12 12:30:56 +0900

    u64を追加

--- include/utility.hpp
+++ include/utility.hpp
@@ -16,6 +16,7 @@
 
 namespace ac_semi_2025::integer_type {
 	using i64 = std::int64_t;
+	using u64 = std::uint64_t;
 	using i32 = std::int32_t;
 	using u32 = std::uint32_t;
 	using u8 = std::uint8_t;
